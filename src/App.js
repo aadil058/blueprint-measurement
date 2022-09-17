1452061f9f9c977f3b218b69334e0d70825c8894
aadil <aadil@wellowise.com>
2022-09-17 11:40:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,226 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageLayer from "ol/layer/Image";
+import Map from "ol/Map";
+import Projection from "ol/proj/Projection";
+import Static from "ol/source/ImageStatic";
+import View from "ol/View";
+import { getCenter } from "ol/extent";
+import { useEffect } from "react";
+import "./App.css";
+
+import Draw from "ol/interaction/Draw";
+import Overlay from "ol/Overlay";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
+import { LineString, Polygon } from "ol/geom";
+import { Vector as VectorSource } from "ol/source";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { getArea, getLength } from "ol/sphere";
+import { unByKey } from "ol/Observable";
 
 function App() {
+  // Map views always need a projection.  Here we just want to map image
+  // coordinates directly to map coordinates, so we create a projection that uses
+  // the image extent in pixels.
+  const extent = [0, 0, 2339, 3308];
+
+  const projection = new Projection({
+    code: "CUSTOM",
+    units: "pixels",
+    extent: extent,
+    global: true,
+    getPointResolution: (resolution, point) => {
+      var pointResolution = resolution;
+      return pointResolution;
+    },
+  });
+
+  useEffect(() => {
+    const source = new VectorSource();
+
+    const vector = new VectorLayer({
+      source: source,
+      style: {
+        "fill-color": "rgba(255, 255, 255, 0.2)",
+        "stroke-color": "#ffcc33",
+        "stroke-width": 2,
+        "circle-radius": 7,
+        "circle-fill-color": "#ffcc33",
+      },
+    });
+
+    const map = new Map({
+      layers: [
+        new ImageLayer({
+          source: new Static({
+            url: "/blueprint.png",
+            projection: projection,
+            imageExtent: extent,
+          }),
+        }),
+        vector,
+      ],
+      target: "map",
+      view: new View({
+        projection: projection,
+        center: getCenter(extent),
+        zoom: 2,
+        maxZoom: 8,
+      }),
+      controls: [],
+    });
+
+    let sketch;
+    let helpTooltipElement;
+    let helpTooltip;
+    let measureTooltipElement;
+    let measureTooltip;
+    const continuePolygonMsg = "Click to continue drawing the polygon";
+    const continueLineMsg = "Click to continue drawing the line";
+    const pointerMoveHandler = function (evt) {
+      if (evt.dragging) {
+        return;
+      }
+      let helpMsg = "Click to start drawing";
+
+      if (sketch) {
+        const geom = sketch.getGeometry();
+        if (geom instanceof Polygon) {
+          helpMsg = continuePolygonMsg;
+        } else if (geom instanceof LineString) {
+          helpMsg = continueLineMsg;
+        }
+      }
+
+      helpTooltipElement.innerHTML = helpMsg;
+      helpTooltip.setPosition(evt.coordinate);
+
+      helpTooltipElement.classList.remove("hidden");
+    };
+
+    map.on("pointermove", pointerMoveHandler);
+
+    map.getViewport().addEventListener("mouseout", function () {
+      helpTooltipElement.classList.add("hidden");
+    });
+
+    let draw;
+
+    const formatLength = function (line) {
+      const length = getLength(line);
+      const image_dpi = 200;
+      const px_to_mm = 25.4 / image_dpi;
+      const scale_arr = [1, 100];
+      return length * px_to_mm * 0.001 * (scale_arr[1] / scale_arr[0]);
+    };
+
+    function addInteraction() {
+      const type = "LineString";
+      draw = new Draw({
+        source: source,
+        type: type,
+        style: new Style({
+          fill: new Fill({
+            color: "rgba(255, 255, 255, 0.2)",
+          }),
+          stroke: new Stroke({
+            color: "rgba(0, 0, 0, 0.5)",
+            lineDash: [10, 10],
+            width: 2,
+          }),
+          image: new CircleStyle({
+            radius: 5,
+            stroke: new Stroke({
+              color: "rgba(0, 0, 0, 0.7)",
+            }),
+            fill: new Fill({
+              color: "rgba(255, 255, 255, 0.2)",
+            }),
+          }),
+        }),
+      });
+
+      map.addInteraction(draw);
+
+      createMeasureTooltip();
+      createHelpTooltip();
+
+      let listener;
+      draw.on("drawstart", function (evt) {
+        sketch = evt.feature;
+
+        let tooltipCoord = evt.coordinate;
+
+        listener = sketch.getGeometry().on("change", function (evt) {
+          const geom = evt.target;
+          console.log("geom", geom);
+          let output;
+          if (geom instanceof LineString) {
+            output = formatLength(geom);
+            tooltipCoord = geom.getLastCoordinate();
+          }
+          measureTooltipElement.innerHTML = output;
+          measureTooltip.setPosition(tooltipCoord);
+        });
+      });
+
+      draw.on("drawend", function () {
+        measureTooltipElement.className = "ol-tooltip ol-tooltip-static";
+        measureTooltip.setOffset([0, -7]);
+        // unset sketch
+        sketch = null;
+        // unset tooltip so that a new one can be created
+        measureTooltipElement = null;
+        createMeasureTooltip();
+        unByKey(listener);
+      });
+    }
+
+    /**
+     * Creates a new help tooltip
+     */
+    function createHelpTooltip() {
+      if (helpTooltipElement) {
+        helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+      }
+      helpTooltipElement = document.createElement("div");
+      helpTooltipElement.className = "ol-tooltip hidden";
+      helpTooltip = new Overlay({
+        element: helpTooltipElement,
+        offset: [15, 0],
+        positioning: "center-left",
+      });
+      map.addOverlay(helpTooltip);
+    }
+
+    /**
+     * Creates a new measure tooltip
+     */
+    function createMeasureTooltip() {
+      if (measureTooltipElement) {
+        measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+      }
+      measureTooltipElement = document.createElement("div");
+      measureTooltipElement.className = "ol-tooltip ol-tooltip-measure";
+      measureTooltip = new Overlay({
+        element: measureTooltipElement,
+        offset: [0, -15],
+        positioning: "bottom-center",
+        stopEvent: false,
+        insertFirst: false,
+      });
+      map.addOverlay(measureTooltip);
+    }
+
+    addInteraction();
+
+    return () => {
+      map.setTarget(undefined);
+      map.dispose();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map-container">
+      <div id="map"></div>
     </div>
   );
 }
